6389589d9b60e6e339b591f00c8d6b5fa49124c2
Duane Northcutt <duane.northcutt@gmail.com>
2026-06-12 09:03:08 -0700

    Moved to AircraftRoute repo.

--- scripts/aircraftInfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Script to get information about a given aircraft identified with an ICAO hex id
-
-HEX_ID=$1
-
-curl -s https://opensky-network.org/api/metadata/aircraft/icao/${HEX_ID} | jq '.'
